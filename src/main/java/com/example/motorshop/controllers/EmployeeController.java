15c406e82fb2530598f3a859d9c982f6476e43a2
Benjamin Orimoloye <bjfikky@yahoo.com>
2020-03-12 22:23:31 -0400

    Added getAllEmployees in EmployeesController

--- src/main/java/com/example/motorshop/controllers/EmployeeController.java
+++ src/main/java/com/example/motorshop/controllers/EmployeeController.java
@@ -1,4 +1,29 @@
 package com.example.motorshop.controllers;
 
+import com.example.motorshop.dao.EmployeeRepository;
+import com.example.motorshop.models.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
 public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping("")
+    public String getAllEmployees(Model model) {
+        List<Employee> employees = (List<Employee>) employeeRepository.findAll();
+        model.addAttribute("employees", employees);
+        return "employees/index";
+    }
 }
--- src/main/java/com/example/motorshop/dao/EmployeeRepository.java
+++ src/main/java/com/example/motorshop/dao/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package com.example.motorshop.dao;
 
-public interface EmployeeRepository {
+import com.example.motorshop.models.Employee;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 }
